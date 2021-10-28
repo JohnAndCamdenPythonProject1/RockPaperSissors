b026b7698b02aa7726eac7dc81fce0b838396254
crlevy1007 <93278710+crlevy1007@users.noreply.github.com>
2021-10-28 14:32:20 -0400

    Add files via upload

--- py/RockPaperScissors.py
+++ py/RockPaperScissors.py
@@ -1,1 +1,3 @@
-print("rock paper scissors game")
+print("Rock Paper Scissors game\nCompete against the AI and gain score ")
+print("-----------------------------------------------------------------")
+print("To play competitive Rock Paper Scissors, you must type Play to continue. ")
