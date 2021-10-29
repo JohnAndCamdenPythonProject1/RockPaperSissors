9d6740926ebae6d7c84b6647b12bf04ab6345c04
crlevy1007 <93278710+crlevy1007@users.noreply.github.com>
2021-10-29 12:40:54 -0400

    updated

--- py/scores.py
+++ py/scores.py
@@ -2,8 +2,18 @@ def scores():
     player_score = 0
     ai_score = 0
     if player_score > ai_score:
-        print("winning")
+        print("Winning")
+        print("Your score is", player_score)
+        print("Computer score is", ai_score)
     elif ai_score > player_score:
         print("losing")
+        print("Your score is", player_score)
+        print("Computer score is", ai_score)
     elif ai_score == player_score:
         print("equal")
+        print("Your score is", player_score)
+        print("Computer score is", ai_score)
+
+
+scores()
+
